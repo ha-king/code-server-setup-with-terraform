22714b99cd8cbd6e3a5c236ae50bda226b71c90a
ha-king <003king@gmail.com>
2026-04-02 13:32:38 +0000

    Fix install script: use explicit path for terraform binary

--- install-terraform.sh
+++ install-terraform.sh
@@ -5,7 +5,8 @@ VERSION="${1:-1.12.0}"
 ZIP="terraform_${VERSION}_linux_amd64.zip"
 
 curl -fsSL "https://releases.hashicorp.com/terraform/${VERSION}/${ZIP}" -o "/tmp/${ZIP}"
+mkdir -p ~/bin
 unzip -o "/tmp/${ZIP}" -d ~/bin
 rm "/tmp/${ZIP}"
 
-terraform version
+~/bin/terraform version
